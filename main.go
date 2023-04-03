57cab15b2362e83a1f189af573514fb0cec318fd
jdcaberoy <jezreeldc@gmail.com>
2023-04-04 02:29:36 +0800

    fix code to ready every 5 mins

--- main.go
+++ main.go
@@ -3,102 +3,94 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	// Prompt user to input specific date
-	var inputDate string
-	fmt.Print("Enter date (dd/mm/yyyy): ")
-	fmt.Scanln(&inputDate)
-	if inputDate == "" {
-		inputDate = time.Now().Format("02/01/2006")
-	}
-
-	// Open CSV file and read data
-	csvFile, err := os.Open("data.csv")
+	// Open CSV file
+	file, err := os.Open("data.csv")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	defer csvFile.Close()
+	defer file.Close()
 
-	csvReader := csv.NewReader(csvFile)
-	headers, err := csvReader.Read()
-	if err != nil {
-		panic(err)
-	}
+	// Initialize start and end times
+	startTime := time.Date(2023, 4, 3, 5, 0, 0, 0, time.UTC)
+	endTime := time.Date(2023, 4, 3, 19, 0, 0, 0, time.UTC)
 
-	// Find index of "Date" and "Value" columns
-	dateIndex := -1
-	valueIndex := -1
-	for i, header := range headers {
-		switch header {
-		case "Date":
-			dateIndex = i
-		case "Value":
-			valueIndex = i
-		}
-	}
-	if dateIndex == -1 || valueIndex == -1 {
-		panic("Missing Date or Value column in CSV file")
+	// Initialize map to store values for each 5-minute interval
+	values := make(map[time.Time][]float64)
+
+	// Read CSV file
+	reader := csv.NewReader(file)
+	reader.Comma = ','
+	reader.Comment = '#'
+	reader.TrimLeadingSpace = true
+	if _, err := reader.Read(); err != nil {
+		log.Fatal(err)
 	}
 
-	// Define start and end times for 5-minute intervals
-	startTime := "05:00:00.000"
-	endTime := "19:00:00.000"
-
-	// Initialize variables to store total and count for each 5-minute interval
-	intervalTotal := 0.0
-	intervalCount := 0
-
-	// Loop through each row in the CSV file
 	for {
-		record, err := csvReader.Read()
-		if err != nil {
+		// Read row
+		row, err := reader.Read()
+		if err == io.EOF {
 			break
+		} else if err != nil {
+			log.Fatal(err)
 		}
 
-		// Parse the row's date string into a time.Time value
-		rowTime, err := time.Parse("02/01/2006 15:04:05", record[dateIndex])
+		// Parse date
+		dateString := row[2]
+		date, err := time.Parse("02/01/2006 15:04:05", dateString)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
-		// Check if the row's date matches the input date
-		if inputDate == rowTime.Format("02/01/2006") {
-
-			// Extract the time component from the row's date string
-			rowTimeString := rowTime.Format("15:04:05.000")
-
-			// Check if the row's time is within the 5-minute interval
-			if startTime <= rowTimeString && rowTimeString <= endTime {
-
-				// Add the row's value to the total and increment the count
-				value, err := strconv.ParseFloat(record[valueIndex], 64)
-				if err != nil {
-					panic(err)
-				}
-				intervalTotal += value
-				intervalCount++
-
-			} else if intervalCount > 0 {
-				// If the row's time is outside the 5-minute interval, calculate the average and print it
-				intervalAverage := intervalTotal / float64(intervalCount)
-				fmt.Printf("Average value for %s-%s: %.2f\n", startTime, endTime, intervalAverage)
-				intervalTotal = 0.0
-				intervalCount = 0
-			}
-
-			// Update start time for next interval
-			startTime = rowTimeString
+		// Skip if date is outside of desired range
+		if date.Before(startTime) || date.After(endTime) {
+			continue
 		}
+
+		// Parse value
+		value, err := strconv.ParseFloat(strings.TrimSpace(row[6]), 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Calculate interval start time
+		intervalStart := time.Date(date.Year(), date.Month(), date.Day(), date.Hour(), (date.Minute()/5)*5, 0, 0, date.Location())
+
+		// Append value to interval
+		values[intervalStart] = append(values[intervalStart], value)
 	}
 
-	// Calculate the average for the last 5-minute interval (if any)
-	if intervalCount > 0 {
-		intervalAverage := intervalTotal / float64(intervalCount)
-		fmt.Printf("Average value for %s-%s: %.2f\n", startTime, endTime, intervalAverage)
+	// Calculate average for each interval
+	for i := startTime; i.Before(endTime); i = i.Add(5 * time.Minute) {
+		intervalStart := i
+		intervalEnd := i.Add(5 * time.Minute)
+
+		if intervalEnd.After(endTime) {
+			intervalEnd = endTime
+		}
+
+		valueList := values[intervalStart]
+
+		if len(valueList) == 0 {
+			fmt.Printf("%s - %s = 0.00\n", intervalStart.Format("15:04:05"), intervalEnd.Format("15:04:05"))
+			continue
+		}
+
+		sum := 0.0
+		for _, value := range valueList {
+			sum += value
+		}
+		avg := sum / float64(len(valueList))
+
+		fmt.Printf("%s - %s = %.2f\n", intervalStart.Format("15:04:05"), intervalEnd.Format("15:04:05"), avg)
 	}
 }
